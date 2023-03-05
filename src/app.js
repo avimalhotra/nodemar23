3080fbf7e1cbc2a49b69377b4df0ae33d4cc066a
Avinash Malhotra <avimalhotra505@gmail.com>
2023-03-05 11:38:09 +0530

    Class One

---
+++ src/app.js
@@ -0,0 +1,19 @@
+//const colors = require('colors');
+import {} from 'colors';
+
+
+console.log("Node JS App".red);
+
+//const math=require('./module');
+
+//console.log(math);
+//console.log(math.area );
+//console.log(math.func() );
+
+//console.log( math );
+
+
+/* ES6 Module */
+import { call } from './module.js';
+
+console.log(call() );
